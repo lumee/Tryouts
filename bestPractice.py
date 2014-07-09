e1153cca751511d004c05acaf26b294f38a506be
lumi <lumi@ubuntu.ubuntu-domain>
2014-07-09 15:15:48 +0300

    first commit

---
+++ bestPractice.py
@@ -0,0 +1,23 @@
+import urllib2
+
+class DataNotFound(Exception):
+    pass
+
+class First(object):
+    def get_data(self, path):
+        req = urllib2.Request(path)
+
+        try:
+            response = urllib2.urlopen(req)
+        except Exception:
+            raise DataNotFound()
+
+        return response.read()
+
+    def post_data(self, destination, data):
+        req = urllib2.Request(destination, data=data)
+        urllib2.urlopen(req)
+
+    def read_file(self, foo):
+        with open(foo, 'r') as f:
+            return f.read()
